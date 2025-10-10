d0b47ada6c03cfdbe8670d85413404eb119bcdd0
emagstrein <emagstrein@tsn.at>
2025-10-10 10:31:56 +0200

    erstellen von php datei und bootstrap.min.css eingebunden

---
+++ SalesPrediction/data/code/index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <title>Sales Prediction</title>
+    <meta charset=utf-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+</head>
+<body>
+
+<h1 class="mt-5 mb-3">Sales Prediction</h1>
+
+<form method="POST" action="index.php">
+
+    <div class="row">
+
+        <div class="col-12">
+
+        </div>
+
+    </div>
+
+</form>
+
+
+
+
+
+</body>
+</html>
+
+<?php
+?>
