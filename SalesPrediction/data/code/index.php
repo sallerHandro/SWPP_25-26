876e351a59af438a36f3f8571456486c143ec98b
emagstrein <emagstrein@tsn.at>
2025-10-13 11:18:28 +0200

    fertigstellen des html formulars mit erster validierung

--- SalesPrediction/data/code/index.php
+++ SalesPrediction/data/code/index.php
@@ -6,25 +6,66 @@
 
 </head>
 <body>
+<div class="container">
 
-<h1 class="mt-5 mb-3">Sales Prediction</h1>
+    <h1 class="mt-5 mb-3">Sales Prediction</h1>
 
-<form method="POST" action="index.php">
+    <form method="POST" action="index.php">
 
-    <div class="row">
+        <div class="row">
 
-        <div class="col-12">
-            <label for="tv">Tv-Budget (in 1000 Dollar)</label>
-            <input type="number"
-                   step="0.01"
-                   min="1"
-                   name="tv"
-                    >
+            <div class="col-12">
+                <label for="tv">Tv-Budget (in 1000 Dollar)</label>
+                <input type="number"
+                       step="0.01"
+                       min="1"
+                       name="tv"
+                       class="form-control"
+                >
+            </div>
+
         </div>
 
-    </div>
+        <div class="row">
 
-</form>
+            <div class="col-12">
+                <label for="radio">Radio-Budget (in 1000 Dollar)</label>
+                <input type="number"
+                       step="0.01"
+                       min="1"
+                       name="radio"
+                       class="form-control"
+                >
+            </div>
+
+        </div>
+
+        <div class="row">
+
+            <div class="col-12">
+                <label for="newspaper">Zeitung-Budget (in 1000 Dollar)</label>
+                <input type="number"
+                       step="0.01"
+                       min="1"
+                       name="newspaper"
+                       class="form-control"
+                >
+            </div>
+
+        </div>
+
+        <div class="row">
+
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary w-100">Vorhersage der Ümsätze</button>
+            </div>
+
+        </div>
+
+    </form>
+
+</div>
+
 
 
 
