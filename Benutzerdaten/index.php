af9274b1f474b6886d0c84916f5a65536d3abc30
emagstrein <emagstrein@tsn.at>
2025-10-17 10:23:17 +0200

    erste html struktur

--- Benutzerdaten/index.php
+++ Benutzerdaten/index.php
@@ -1,3 +1,49 @@
 <html>
+<head>
+    <title>Benutzerdaten</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
 
+        <h1 class="mt-5 mb-3">Benutzerdaten</h1>
+
+        <form method="post">
+
+            <div class="row">
+
+                <div class="col-1">
+                    <label for="suche" class="col-form-label">Suche: </label>
+                </div>
+
+                <div class="col-4">
+                    <input type="text"
+                           class="form-control"
+                           id="suche"
+                           name="suche">
+                </div>
+
+                <div class="col-1">
+                    <input type="submit"
+                           class="btn btn-primary"
+                           value="Suchen">
+                </div>
+
+                <div class="col-1">
+                    <a href="index.php" class="btn btn-secondary btn-block">Leeren</a>
+                </div>
+
+            </div>
+
+            <div class="row">
+
+            </div>
+
+        </form>
+
+
+
+    </div>
+</body>
 </html>
